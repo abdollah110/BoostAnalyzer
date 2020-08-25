4510f7566230e75cc3431377baa6f7223c9fd881
abdollah <smohamadi@gmail.com>
2020-08-25 18:48:09 -0500

    Boosted Embeder

--- plugins/ntuplizer_boostedTaus.cc
+++ plugins/ntuplizer_boostedTaus.cc
@@ -354,15 +354,15 @@ void BoostAnalyzer::fillBoostedTaus(const edm::Event& e)
     for(vector<pat::Tau>::const_iterator ibtau_new = boostedTauHandleNew->begin(); ibtau_new != boostedTauHandleNew->end(); ++ibtau_new) {
         std::cout<<"itau->tauID('chargedIsoPtSum') " << ibtau_new->tauID("chargedIsoPtSum")<<"\n";
         
-        std::cout<<itau->signalChargedHadrCands().size()<<"\n";
-        std::cout<<itau->signalNeutrHadrCands().size()<<"\n";
-        std::cout<<itau->signalGammaCands().size()<<"\n";
-        std::cout<<itau->signalCands().size()<<"\n";
+        std::cout<<ibtau_new->signalChargedHadrCands().size()<<"\n";
+        std::cout<<ibtau_new->signalNeutrHadrCands().size()<<"\n";
+        std::cout<<ibtau_new->signalGammaCands().size()<<"\n";
+        std::cout<<ibtau_new->signalCands().size()<<"\n";
         
-        std::cout<<itau->isolationChargedHadrCands().size()<<"\n";
-        std::cout<<itau->isolationNeutrHadrCands().size()<<"\n";
-        std::cout<<itau->isolationGammaCands().size()<<"\n";
-        std::cout<<itau->isolationCands().size()<<"\n";
+        std::cout<<ibtau_new->isolationChargedHadrCands().size()<<"\n";
+        std::cout<<ibtau_new->isolationNeutrHadrCands().size()<<"\n";
+        std::cout<<ibtau_new->isolationGammaCands().size()<<"\n";
+        std::cout<<ibtau_new->isolationCands().size()<<"\n";
 
     }
 

f090c075c3b4c9937e8cee6c4ea9522462f94d96
abdollah <smohamadi@gmail.com>
2020-10-01 18:06:37 -0500

    Add IsoPtSum

--- plugins/PATBoostedTauEmbedder.cc
+++ plugins/PATBoostedTauEmbedder.cc
@@ -250,16 +250,16 @@ void PATBoostedTauEmbedder::produce(edm::Event& evt, const edm::EventSetup& es)
                     if (charged->pt() <= 0.5) continue;
                     
 //                  if (0)  std::cout<<"tauVertexIdx = "<<tauVertexIdx<<"\n";
-                    if (std::abs(charged->dxy(*vertices[tauVertexIdx].position())) >= 0.03) continue;
+//                    if (std::abs(charged->dxy(*vertices[tauVertexIdx].position())) >= 0.03) continue;
                     const reco::Track *track = charged->bestTrack();
                     if (track == nullptr) continue;
-//                      if (std::abs(track->dxy(*vertices[tauVertexIdx].position())) >= 0.03) continue;
+                      if (std::abs(track->dxy(*vertices[tauVertexIdx].position())) >= 0.03) continue;
                     if (track->normalizedChi2() >= 100) continue;
 //                    if (track->numberOfHits() < 3) continue;
                     if (track->numberOfValidHits() < 3) continue; //????
                     
-                    double dz = std::abs(charged->dz(*vertices[tauVertexIdx].position()));
-//                    double dz = std::abs(track->dz(*vertices[tauVertexIdx].position()));
+//                    double dz = std::abs(charged->dz(*vertices[tauVertexIdx].position()));
+                    double dz = std::abs(track->dz(*vertices[tauVertexIdx].position()));
                     double dR = deltaR(charged->p4(), tau.p4());
                     if (dz < 0.2) {//from tau vertex
                       //iso cone

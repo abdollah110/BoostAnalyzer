99b387ca0a8a5862b09cd2eda846c63153d04edd
abdollah <smohamadi@gmail.com>
2020-10-29 13:41:17 -0500

    dubug antiEle

--- python/runCleanedBoostedTauIdMVA.py
+++ python/runCleanedBoostedTauIdMVA.py
@@ -986,11 +986,11 @@ class BoostedTauIDEmbedder(object):
                 _againstElectronTauIDSources
             )
             tauIDSources =_tauIDSourcesWithAgainistEle.clone()
-            print "tauIDSources 1 ", tauIDSources.size()
+            print "tauIDSources 1 ", tauIDSources
             
 
         ##
-        print "tauIDSources 2 ", tauIDSources.size()
+        print "tauIDSources 2 ", tauIDSources
         print('Embedding new TauIDs into \"'+self.updatedTauName+'\"')
         embedID = self.cms.EDProducer("PATBoostedTauIDEmbedder",
             src = self.PATTauProducer,
